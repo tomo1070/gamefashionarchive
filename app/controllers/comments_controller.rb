1da8089528882f140f3f5a3d6c295d8c833d2007
tomo1070 <160234199+tomo1070@users.noreply.github.com>
2024-06-15 12:15:17 +0000

    コメント完了

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,23 +2,24 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-    @comment.post = @post
+    @comment.post_id = @post.id
+    @comment.user_id = current_user.id
 
     if @comment.save
-      redirect_to post_path(@post), notice: "コメントが投稿されました。"
+      redirect_to list_path(@post), notice: "コメントが投稿されました。"
     else
-      redirect_to post_path(@post), alert: "コメントの投稿に失敗しました。"
+      redirect_to list_path(@post), alert: "コメントの投稿に失敗しました。"
     end
   end
-  
+
   def destroy
     Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    redirect_to list_path(params[:post_id])
   end
-  
+
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:comment)
   end
 end
