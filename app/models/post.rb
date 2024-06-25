a0c7dc2db433f80da5f47bedfb9e6cfef4f91e0f
tomo1070 <160234199+tomo1070@users.noreply.github.com>
2024-06-25 07:09:54 +0000

    いいね作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :tags
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   attribute :title, :string
   
@@ -16,5 +17,8 @@ class Post < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
+  end
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 end
