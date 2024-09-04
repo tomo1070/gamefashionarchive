ea8e61f021ac6016ee8d306a401c0d91d5d403e9
tomo1070 <160234199+tomo1070@users.noreply.github.com>
2024-09-04 07:07:48 +0000

    卒業後2

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Gamefashionarchive
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    
+
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -19,5 +19,6 @@ module Gamefashionarchive
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
   end
 end
